d64684943c6d03ae70399a6c329ba3671071f00b
industry1111 <industry1111@naver.com>
2023-08-11 18:50:43 +0900

    docs : add QueryDSL

--- src/main/java/com/mailpug/homework/common/Appconfig.java
+++ src/main/java/com/mailpug/homework/common/Appconfig.java
@@ -1,5 +1,7 @@
 package com.mailpug.homework.common;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,9 +9,10 @@ import javax.persistence.EntityManager;
 
 @Configuration
 public class Appconfig {
-//    @Bean
-//    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-//        return new JPAQueryFactory(em);
-//    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 
 }
