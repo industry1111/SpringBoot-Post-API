9f8e74da4eadafdecbe51b5996899fe59bd60b26
industry1111 <industry1111@naver.com>
2023-08-12 22:09:17 +0900

    docs : QueryDSL사용을 위한 쿼리프로젝션 및 컴포넌트 스캔 어노테이션 추가

--- src/main/java/com/mailpug/homework/HomeworkApplication.java
+++ src/main/java/com/mailpug/homework/HomeworkApplication.java
@@ -1,11 +1,14 @@
 package com.mailpug.homework;
 
+import com.mailpug.homework.common.Appconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackageClasses = Appconfig.class)
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
